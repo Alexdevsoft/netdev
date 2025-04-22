ca567384288048e1683779384d2e91eab5bb7f5f
Alexdevsoft <alexhavilla2022@gmail.com>
2025-04-21 22:42:10 -0300

    Classe Entity criada

---
+++ src/main/java/netdev/entity/User.java
@@ -0,0 +1,17 @@
+package netdev.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;  // Hash com BCrypt
+    private String bio;
+}
