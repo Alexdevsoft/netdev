d5034d515d448abb202020ca8fd347cef0962477
Alexdevsoft <alexhavilla2022@gmail.com>
2025-04-21 22:17:08 -0300

    Netdev criado

---
+++ src/main/java/netdev/NetdevApplication.java
@@ -0,0 +1,13 @@
+package netdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetdevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/netdev/NetdevApplicationTests.java
@@ -0,0 +1,13 @@
+package netdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
